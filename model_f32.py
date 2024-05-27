2cf802896c98b7cf17cc60593ca8a7f4aa938e22
Gonçalo Gouveia <68733294+gongouveia@users.noreply.github.com>
2024-05-27 20:26:26 +0100

    Add files via upload

---
+++ model_f32.py
@@ -0,0 +1,38 @@
+import util.model_util as model_util
+import util.dataset_util as dataset_util
+
+
+import torch
+import torch.nn as nn
+import torch.nn.quantized as nnq
+
+
+# Other
+DEVICE = "cuda"
+
+
+
+
+model_path = 'saved_model.pt'
+
+model = model_util.load_pytorch_script_model(model_path)
+
+
+
+if model:
+    # You can now use the model for inference or further processing
+    print("Model is ready for use.")
+else:
+    print("Model could not be loaded.")
+
+
+
+
+train_dataset, test_dataset, train_loader, test_loader =dataset_util.dataset_load()
+
+
+
+with torch.no_grad(): # save memory during inference
+    print('Test accuracy: %.2f%%' % (model_util.compute_accuracy(model, test_loader, device=DEVICE)))
+
+
---
+++ quantize_f16.py
@@ -0,0 +1,46 @@
+
+import util.model_util as model_util
+import util.dataset_util as dataset_util
+
+
+
+import torch
+import torch.nn as nn
+import torch.nn.quantized as nnq
+
+
+# Other
+DEVICE = "cuda"
+
+
+model_path = 'saved_model.pt'
+model_path_half = 'saved_model_half.pt'
+
+model = model_util.load_pytorch_script_model(model_path)
+
+
+
+if model:
+    # You can now use the model for inference or further processing
+    print("Model is ready for use.")
+else:
+    print("Model could not be loaded.")
+
+
+
+
+train_dataset, test_dataset, train_loader, test_loader =dataset_util.dataset_load()
+
+
+
+with torch.no_grad(): # save memory during inference
+    print('Test accuracy: %.2f%%' % (model_util.compute_accuracy_half(model, test_loader, device=DEVICE)))
+
+
+try:
+
+    model_util.save_model_script(model.half(), model_path_half)
+    print('model 16bit saved')
+
+except:
+    print('ERROR')

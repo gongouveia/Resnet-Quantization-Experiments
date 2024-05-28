8cd242677920a2654311f321ff5420f081f03da8
Gonçalo Gouveia <68733294+gongouveia@users.noreply.github.com>
2024-05-28 09:21:58 +0100

    Add files via upload

---
+++ util/hyperparameters.py
@@ -0,0 +1,17 @@
+
+
+
+
+# Hyperparameters
+RANDOM_SEED = 1
+LEARNING_RATE = 0.001
+BATCH_SIZE = 128
+NUM_EPOCHS = 5
+
+# Architecture
+NUM_FEATURES = 28*28
+NUM_CLASSES = 10
+
+# Other
+DEVICE = "cuda"
+GRAYSCALE = True

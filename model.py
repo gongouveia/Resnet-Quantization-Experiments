3f1e83d5833cb9ae795bcb7bde0d634ea4bf34a2
yidoashiyi <925367077@qq.com>
2024-10-14 15:17:40 +0800

    fix PTQ bugs and add resnet50 model

--- model.py
+++ model.py
@@ -121,3 +121,51 @@ def print_model_info(model):
     print(f"Model size (in bytes): {model_size}")
 
 
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+        self.add_relu = nn.ReLU(inplace=True)
+        self.skip_add = nn.quantized.FloatFunctional()
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out = self.skip_add.add(out, residual)
+        out = self.add_relu(out)
+
+        return out
+
+def quantizable_resnet50(num_classes, grayscale):
+    """Constructs a ResNet-50 model."""
+    model = ResNet(block=Bottleneck, layers=[3, 4, 6, 3], num_classes=num_classes, grayscale=grayscale, quantize=True)
+    return model
+
+def resnet50(num_classes, grayscale):
+    """Constructs a ResNet-50 model."""
+    model = ResNet(block=Bottleneck, layers=[3, 4, 6, 3], num_classes=num_classes, grayscale=grayscale, quantize=False)
+    return model
--- quantize_int8_PTQ.py
+++ quantize_int8_PTQ.py
@@ -49,17 +49,15 @@ model_fused = torch.quantization.fuse_modules(model,
                                                ['layer4.1.conv1', 'layer4.1.bn1', 'layer4.1.relu'],
                                                ['layer4.1.conv2', 'layer4.1.bn2']])
 
-# Quantize the model
-model_prepared = tq.prepare(model_fused)
-model_quantized = tq.convert(model_prepared)
-
-
 
 # Define the quantization configuration
 quant_config = tq.get_default_qconfig('fbgemm')
 model_fused.qconfig = quant_config
 
 
+# Quantize the model
+model_prepared = tq.prepare(model_fused)
+model_quantized = tq.convert(model_prepared)
 
 train_dataset, test_dataset, train_loader, test_loader = util.dataset_util.dataset_load()
 
